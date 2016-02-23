7dc328083cab6c3f3abbfbfa91178b7ed8a2ef76
Vaspol Ruamviboonsuk <vaspol@umich.edu>
2016-02-23 12:42:09 -0500

    Added configure and make scripts

---
+++ make_script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export PYTHONPATH='/Users/vaspol/Documents/research/MobileWebPageOptimization/nghttp2_dev/build/lib/python2.7/site-packages/'
+make all
+make install # This should install it to .../nghttp2_dev/build/
